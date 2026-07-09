0ba1b5deafa59702abe3afbb14ad80faaefa7eb8
Ganesh <ganeshgarje716@gmail.com>
2026-07-09 23:21:22 +0530

    Create Variables With Validations

--- Spring-Security-Product/src/main/java/com/ganesh/dto/ProductDto.java
+++ Spring-Security-Product/src/main/java/com/ganesh/dto/ProductDto.java
@@ -1,8 +1,41 @@
 package com.ganesh.dto;
 
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ProductDto {
+	
+	
+	
+	private Integer id;
+	
+	@NotNull(message = "Name Is Required")
+	private String name;
+	
+	@NotNull(message = "Price Is Required")
+	private Double price;
+	
+	@NotNull(message = "Discount Is Required")
+	private Double discount;
+	
+	@NotNull(message = "Stock Is Required")
+	private Integer stock;
+	
+	@NotNull(message = "Category Is Required")
+	private String category;
+	
+	@NotNull(message = "Brand Is Required")
+	private String brand;
+	
+	@NotNull(message = "Rating Is Required")
+	private Double rating;
+	
+	@NotNull(message = "Available Is Required")
+	private Boolean available;
+	
+	private LocalDate createdDate;
 
 }
