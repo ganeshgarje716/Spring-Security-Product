bb956a7bdce44ead12c6442661992412b1669018
Ganesh <ganeshgarje716@gmail.com>
2026-07-09 23:18:32 +0530

    Add Variables With Annotations

--- Spring-Security-Product/src/main/java/com/ganesh/entity/Product.java
+++ Spring-Security-Product/src/main/java/com/ganesh/entity/Product.java
@@ -1,6 +1,13 @@
 package com.ganesh.entity;
 
+import java.time.LocalDate;
+
+import org.hibernate.annotations.CreationTimestamp;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,5 +17,30 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 public class Product {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String name;
+	
+	private Double price;
+	
+	private Double discount;
+	
+	private Integer stock;
+	
+	private String category;
+	
+	private String brand;
+	
+	private Double rating;
+	
+	private Boolean available;
+	
+	@CreationTimestamp
+	private LocalDate createdDate;
+	
 
 }
