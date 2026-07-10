5c578302b1615cb708ee29cfd9947898b41cc4d8
Ganesh <ganeshgarje716@gmail.com>
2026-07-10 23:48:02 +0530

    Create ProductNotFoundException Class for Handling Exceptions

---
+++ Spring-Security-Product/src/main/java/com/ganesh/exception/ProductNotFoundException.java
@@ -0,0 +1,13 @@
+package com.ganesh.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+	
+	
+	public ProductNotFoundException(String msg) {
+		
+		super(msg);
+		
+		// TODO Auto-generated constructor stub
+	}
+
+}
