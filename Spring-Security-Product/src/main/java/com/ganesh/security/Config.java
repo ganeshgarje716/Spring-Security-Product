ce64362bf1befaead006e26c9a55f96b6df2c9c7
Ganesh <ganeshgarje716@gmail.com>
2026-07-11 18:08:45 +0530

    Create Security For Handling API

--- Spring-Security-Product/src/main/java/com/ganesh/security/Config.java
+++ Spring-Security-Product/src/main/java/com/ganesh/security/Config.java
@@ -1,10 +1,69 @@
 package com.ganesh.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.ganesh.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class Config {
+	
+	
+	
+	private final CustomUserDetailsService customUserDetailsService;
+	
+	
+	public Config(CustomUserDetailsService customUserDetailsService) {
+		
+		this.customUserDetailsService=customUserDetailsService;
+	}
+	
+	
+	
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	
+	@Bean
+	public void authenticateUser() {
+		
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider(customUserDetailsService);
+		
+		provider.setPasswordEncoder(encoder());
+	}
+	
+	
+	
+	public void filterChain(HttpSecurity http) {
+		
+		http.csrf(csrf->csrf.disable());
+		
+		http.httpBasic(Customizer.withDefaults());
+		
+		http.authorizeHttpRequests(auth->{
+			
+			auth.requestMatchers("/api/v1/product/save").hasRole("ADMIN");
+			
+			auth.requestMatchers("/api/v1/product/all").hasAnyRole("ADMIN", "USER");
+			
+			auth.requestMatchers("/api/v1/product/byid/**").hasAnyRole("ADMIN", "USER");
+			
+			auth.requestMatchers("/api/v1/product/update/**").hasRole("ADMIN");
+			
+			auth.requestMatchers("/api/v1/product/delete/**").hasRole("ADMIN");
+			
+			auth.requestMatchers("/auth/create-user").permitAll();
+		});
+	}
 
 }
