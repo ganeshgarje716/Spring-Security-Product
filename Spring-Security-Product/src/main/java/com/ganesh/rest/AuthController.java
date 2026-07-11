eaff10dee5a93a48bed2d02a9745375351ad6c09
Ganesh <ganeshgarje716@gmail.com>
2026-07-11 17:53:38 +0530

    Create createUsers() Method

--- Spring-Security-Product/src/main/java/com/ganesh/rest/AuthController.java
+++ Spring-Security-Product/src/main/java/com/ganesh/rest/AuthController.java
@@ -1,7 +1,14 @@
 package com.ganesh.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ganesh.entity.User;
+import com.ganesh.repository.UserRepository;
 
 @RestController
 @RequestMapping("/auth")
@@ -9,4 +16,37 @@ public class AuthController {
 	
 	
 
+	private final UserRepository userRepository;
+	
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	
+	public AuthController(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+		
+		this.userRepository=userRepository;
+		
+		this.bCryptPasswordEncoder=bCryptPasswordEncoder;
+	}
+	
+	
+	
+	
+	public ResponseEntity<User> createUsers(@RequestBody User user) {
+		
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		
+		if (user.getUsername().equals("Ganesh")) {
+			
+			user.setRole("ADMIN");
+		}
+		else {
+			
+			user.setRole("USER");
+		}
+		
+		User saved = userRepository.save(user);
+		
+		return new ResponseEntity<User>(saved, HttpStatus.CREATED);
+	}
+	
 }
--- Spring-Security-Product/src/main/java/com/ganesh/service/CustomUserDetailsService.java
+++ Spring-Security-Product/src/main/java/com/ganesh/service/CustomUserDetailsService.java
@@ -1,15 +1,46 @@
 package com.ganesh.service;
 
+import java.util.Optional;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.ganesh.entity.User;
+import com.ganesh.repository.UserRepository;
+
 public class CustomUserDetailsService implements UserDetailsService{
+	
+	
+	
+	private final UserRepository userRepository;
+	
+	
+	public CustomUserDetailsService(UserRepository userRepository) {
+		
+		this.userRepository=userRepository;
+	}
+	
+	
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Optional<User> byUsername = userRepository.findByUsername(username);
+		
+		if (byUsername.isPresent()) {
+			
+			User user = byUsername.get();
+			
+			return org.springframework.security.core.userdetails.User
+					.withUsername(username)
+					.password(user.getPassword())
+					.roles(user.getRole())
+					.build();
+		}
+		
+		
+		throw new UsernameNotFoundException("User Not Found With UserName = "+username);
 	}
 
 }
